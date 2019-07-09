2dc2772d63d5fdab6e64a64fb10d6262dfb8ddb8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-09 13:59:13 +0200

    add extra task 1

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 const convert = require('color-convert');
+const fs = require('fs');
 const port = 3000
+
+const addToStatistics = (req, res, next) => {
+  if ("color" in req.query) {
+    const { color } = req.query;
+    const exists = fs.existsSync("statistic.json");
+    let statistics = { colorStatistics: {} };
+    if (exists) {
+      statistics = JSON.parse(fs.readFileSync("statistic.json", "utf8"));
+    }
+    if (color in statistics.colorStatistics) {
+      statistics.colorStatistics[color] = statistics.colorStatistics[color] + 1;
+    } else {
+      statistics.colorStatistics[color] = 1;
+    }
+    fs.writeFileSync("statistic.json", JSON.stringify(statistics), { encoding: "utf8" })
+  }
+  next();
+}
+
+app.use(addToStatistics);
 
 app.get('/convert/keywordtorgb', (req, res) => {
   const color = convert.keyword.rgb(req.query.color)
@@ -26,5 +47,6 @@ app.get('/convert/hextorgb', (req, res) => {
   const [red, green, blue] = convert.hex.rgb(hex);
   return res.json({ red, green, blue });
 });
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
