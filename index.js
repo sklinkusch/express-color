7ad2f36954b137a4778ef91138af7735fd1914e5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-09 13:07:19 +0200

    finished hextorgb

--- index.js
+++ index.js
@@ -9,16 +9,22 @@ app.get('/convert/keywordtorgb', (req, res) => {
   return res.json({ red, green, blue })
 });
 app.get('/convert/rgbtohsl', (req, res) => {
-  const { r, g, b } = req.query;
+  const { red, green, blue } = req.query;
   const color = convert.rgb.hsl(r, g, b);
   const [hue, saturation, luminance] = color;
   return res.json({ hue, saturation, luminance });
 });
 
 app.get('/convert/rgbtohex', (req, res) => {
-  const { r, g, b } = req.query;
+  const { red, green, blue } = req.query;
   const hex = convert.rgb.hex(r, g, b);
   return res.json({ hex });
+});
+
+app.get('/convert/hextorgb', (req, res) => {
+  const { hex } = req.query;
+  const [red, green, blue] = convert.hex.rgb(hex);
+  return res.json({ red, green, blue });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
