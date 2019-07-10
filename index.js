7ed1d11d03612aa8764d54c8f60465470f2f9894
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 19:47:37 +0200

    change import

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const app = express()
-const convertRoute = require('./router/convert');
-const { readStatistics, logMethod } = require("./controller/converter");
+const express = require("express");
+const app = express();
+const convertRoute = require("./router/convert");
+const { logMethod } = require("./controller/converter");
+// const { readStatistics } = require("./controller/converter");
+const { readStatistics } = require("./model/statistics");
 
-app.use(logMethod)
-app.use('/convert', convertRoute)
-app.get('/statistics', readStatistics);
+app.use(logMethod);
+app.use("/convert", convertRoute);
+app.get("/statistics", readStatistics);
 
-const port = 3000
+const port = 3000;
 
-
-
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
