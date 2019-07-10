95504b71de9035ae35db76f6cee086ca0bac177a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 19:35:32 +0200

    write function addToStatistics based on lowdb

--- model/statistics.js
+++ model/statistics.js
@@ -1,10 +1,10 @@
-const lowdb = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('db.json')
-const db = lowdb(adapter)
-
-db.defaults({ colorStatistics: {} }).write()
+const lowdb = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = lowdb(adapter);
 
 exports.addToStatistics = (req, res, next) => {
-}
-
+  const { color } = req.query;
+  db.update(`colorStatistics.${color}`, n => n + 1 || 1).write();
+  next();
+};
