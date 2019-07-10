6d217ee59639e113f41ab351c8be1891a8c878b2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 11:59:35 +0200

    change middleware

--- router/convert.js
+++ router/convert.js
@@ -2,10 +2,10 @@ const express = require('express')
 const convertRoute = express.Router()
 const { keywordRGBHandler, RGB2HSLHandler, RGB2HexHandler, Hex2RGBHandler, addToStatistics } = require('../controller/converter');
 
-convertRoute.use(addToStatistics);
-convertRoute.get('/convert/keywordtorgb', keywordRGBHandler);
-convertRoute.get('/convert/rgbtohsl', RGB2HSLHandler);
-convertRoute.get('/convert/rgbtohex', RGB2HexHandler);
-convertRoute.get('/convert/hextorgb', Hex2RGBHandler);
+convertRoute.use('/keywordtorgb', addToStatistics);
+convertRoute.get('/keywordtorgb', keywordRGBHandler);
+convertRoute.get('/rgbtohsl', RGB2HSLHandler);
+convertRoute.get('/rgbtohex', RGB2HexHandler);
+convertRoute.get('/hextorgb', Hex2RGBHandler);
 
 module.exports = convertRoute;
