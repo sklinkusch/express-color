9a3e8cc6041f345a5f6daa336e454644936aa72b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 11:11:06 +0200

    copied handlers to converter.js

---
+++ controller/converter.js
@@ -0,0 +1,49 @@
+exports.keywordRGBHandler = (req, res) => {
+  const color = convert.keyword.rgb(req.query.color)
+  const [red, green, blue] = color
+  return res.json({ red, green, blue })
+}
+
+exports.RGB2HSLHandler = (req, res) => {
+  const { r, g, b } = req.query;
+  const color = convert.rgb.hsl(r, g, b);
+  const [hue, saturation, luminance] = color;
+  return res.json({ hue, saturation, luminance });
+}
+
+exports.RGB2HexHandler = (req, res) => {
+  const { r, g, b } = req.query;
+  const hex = convert.rgb.hex(r, g, b);
+  return res.json({ hex });
+}
+
+exports.Hex2RGBHandler = (req, res) => {
+  const { hex } = req.query;
+  const [r, g, b] = convert.hex.rgb(hex);
+  return res.json({ red, green, blue });
+}
+
+exports.readStatistics = (req, res) => {
+  const fs = require('fs');
+  const statistics = JSON.parse(fs.readFileSync('../statistic.json', 'utf8'));
+  return res.json({ statistics })
+}
+
+exports.addToStatistics = (req, res, next) => {
+  const fs = require('fs');
+  if ("color" in req.query) {
+    const { color } = req.query;
+    const exists = fs.existsSync("../statistic.json");
+    let statistics = { colorStatistics: {} };
+    if (exists) {
+      statistics = JSON.parse(fs.readFileSync("../statistic.json", "utf8"));
+    }
+    if (color in statistics.colorStatistics) {
+      statistics.colorStatistics[color] = statistics.colorStatistics[color] + 1;
+    } else {
+      statistics.colorStatistics[color] = 1;
+    }
+    fs.writeFileSync("../statistic.json", JSON.stringify(statistics), { encoding: "utf8" })
+  }
+  next();
+}
--- index.js
+++ index.js
@@ -1,57 +1,16 @@
 const express = require('express')
 const app = express()
 const convert = require('color-convert');
-const fs = require('fs');
+const { keywordRGBHandler, RGB2HSLHandler, RGB2HexHandler, Hex2RGBHandler, readStatistics, addToStatistics } = require('./controller/converter');
 const port = 3000
-
-const addToStatistics = (req, res, next) => {
-  if ("color" in req.query) {
-    const { color } = req.query;
-    const exists = fs.existsSync("statistic.json");
-    let statistics = { colorStatistics: {} };
-    if (exists) {
-      statistics = JSON.parse(fs.readFileSync("statistic.json", "utf8"));
-    }
-    if (color in statistics.colorStatistics) {
-      statistics.colorStatistics[color] = statistics.colorStatistics[color] + 1;
-    } else {
-      statistics.colorStatistics[color] = 1;
-    }
-    fs.writeFileSync("statistic.json", JSON.stringify(statistics), { encoding: "utf8" })
-  }
-  next();
-}
 
 app.use(addToStatistics);
 
-app.get('/convert/keywordtorgb', (req, res) => {
-  const color = convert.keyword.rgb(req.query.color)
-  const [red, green, blue] = color
-  return res.json({ red, green, blue })
-});
-app.get('/convert/rgbtohsl', (req, res) => {
-  const { r, g, b } = req.query;
-  const color = convert.rgb.hsl(r, g, b);
-  const [hue, saturation, luminance] = color;
-  return res.json({ hue, saturation, luminance });
-});
-
-app.get('/convert/rgbtohex', (req, res) => {
-  const { r, g, b } = req.query;
-  const hex = convert.rgb.hex(r, g, b);
-  return res.json({ hex });
-});
-
-app.get('/convert/hextorgb', (req, res) => {
-  const { hex } = req.query;
-  const [r, g, b] = convert.hex.rgb(hex);
-  return res.json({ red, green, blue });
-});
-
-app.get('/statistics', (req, res) => {
-  const statistics = JSON.parse(fs.readFileSync('statistic.json', 'utf8'));
-  return res.json({ statistics })
-});
+app.get('/convert/keywordtorgb', keywordRGBHandler);
+app.get('/convert/rgbtohsl', RGB2HSLHandler);
+app.get('/convert/rgbtohex', RGB2HexHandler);
+app.get('/convert/hextorgb', Hex2RGBHandler);
+app.get('/statistics', readStatistics);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
