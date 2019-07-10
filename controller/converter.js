43514c5dff06230c322f3fbd949aab551a625a3d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 19:48:06 +0200

    comment out old statistic handler

--- controller/converter.js
+++ controller/converter.js
@@ -1,62 +1,68 @@
-const convert = require('color-convert');
+const convert = require("color-convert");
 
 exports.keywordRGBHandler = (req, res) => {
-  const color = convert.keyword.rgb(req.query.color)
-  const [red, green, blue] = color
-  return res.json({ red, green, blue })
-}
+  const color = convert.keyword.rgb(req.query.color);
+  const [red, green, blue] = color;
+  return res.json({ red, green, blue });
+};
 
 exports.RGB2HSLHandler = (req, res) => {
-  const colorInput = req.query.color.split(",").map(channel => parseInt(channel));
+  const colorInput = req.query.color
+    .split(",")
+    .map(channel => parseInt(channel));
   const [hue, saturation, luminance] = convert.rgb.hsl(...colorInput);
   return res.json({ hue, saturation, luminance });
-}
+};
 
 exports.RGB2HexHandler = (req, res) => {
-  const colorInput = req.query.color.split(",").map(channel => parseInt(channel));
+  const colorInput = req.query.color
+    .split(",")
+    .map(channel => parseInt(channel));
   const hex = convert.rgb.hex(...colorInput);
   return res.json({ hex });
-}
+};
 
 exports.Hex2RGBHandler = (req, res) => {
   const { color: hex } = req.query;
   const [r, g, b] = convert.hex.rgb(hex);
   return res.json({ red: r, green: g, blue: b });
-}
+};
 
-exports.readStatistics = (req, res) => {
-  const fs = require('fs');
-  const statistics = JSON.parse(fs.readFileSync('./statistic.json', 'utf8'));
-  return res.json({ statistics })
-}
+// exports.readStatistics = (req, res) => {
+//   const fs = require("fs");
+//   const statistics = JSON.parse(fs.readFileSync("./statistic.json", "utf8"));
+//   return res.json({ statistics });
+// };
 
-exports.addToStatistics = (req, res, next) => {
-  const fs = require('fs');
-  const { color } = req.query;
-  const exists = fs.existsSync("./statistic.json");
-  let statistics = { colorStatistics: {} };
-  if (exists) {
-    statistics = JSON.parse(fs.readFileSync("./statistic.json", "utf8"));
-  }
-  if (color in statistics.colorStatistics) {
-    statistics.colorStatistics[color] = statistics.colorStatistics[color] + 1;
-  } else {
-    statistics.colorStatistics[color] = 1;
-  }
-  fs.writeFileSync("./statistic.json", JSON.stringify(statistics), { encoding: "utf8" })
-  next();
-}
+// exports.addToStatistics = (req, res, next) => {
+//   const fs = require('fs');
+//   const { color } = req.query;
+//   const exists = fs.existsSync("./statistic.json");
+//   let statistics = { colorStatistics: {} };
+//   if (exists) {
+//     statistics = JSON.parse(fs.readFileSync("./statistic.json", "utf8"));
+//   }
+//   if (color in statistics.colorStatistics) {
+//     statistics.colorStatistics[color] = statistics.colorStatistics[color] + 1;
+//   } else {
+//     statistics.colorStatistics[color] = 1;
+//   }
+//   fs.writeFileSync("./statistic.json", JSON.stringify(statistics), { encoding: "utf8" })
+//   next();
+// }
 
 exports.logMethod = (req, res, next) => {
-  const { method, url } = req.socket.parser.incoming
-  console.log(`${method}: ${url}`)
-  next()
-}
+  const { method, url } = req.socket.parser.incoming;
+  console.log(`${method}: ${url}`);
+  next();
+};
 
 exports.ColorInQuery = (req, res, next) => {
   if ("color" in req.query) {
     next();
   } else {
-    res.status(422).json({ error: { message: "missing query parameter 'color'" } })
+    res
+      .status(422)
+      .json({ error: { message: "missing query parameter 'color'" } });
   }
-}
+};
