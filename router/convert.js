f4ff700f33ff1f3d9ea452ac1427c0c1ec4ccf87
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-10 19:48:27 +0200

    replace import

--- router/convert.js
+++ router/convert.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const convertRoute = express.Router()
-const { keywordRGBHandler, RGB2HSLHandler, RGB2HexHandler, Hex2RGBHandler, addToStatistics, ColorInQuery } = require('../controller/converter');
+const express = require("express");
+const convertRoute = express.Router();
+const {
+  keywordRGBHandler,
+  RGB2HSLHandler,
+  RGB2HexHandler,
+  Hex2RGBHandler,
+  ColorInQuery
+} = require("../controller/converter");
+const { addToStatistics } = require("../model/statistics");
 
 convertRoute.use(ColorInQuery);
-convertRoute.use('/keywordtorgb', addToStatistics);
-convertRoute.get('/keywordtorgb', keywordRGBHandler);
-convertRoute.get('/rgbtohsl', RGB2HSLHandler);
-convertRoute.get('/rgbtohex', RGB2HexHandler);
-convertRoute.get('/hextorgb', Hex2RGBHandler);
+convertRoute.use("/keywordtorgb", addToStatistics);
+convertRoute.get("/keywordtorgb", keywordRGBHandler);
+convertRoute.get("/rgbtohsl", RGB2HSLHandler);
+convertRoute.get("/rgbtohex", RGB2HexHandler);
+convertRoute.get("/hextorgb", Hex2RGBHandler);
 
 module.exports = convertRoute;
