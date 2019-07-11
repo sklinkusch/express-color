bba50fcd3a8646423dca86df2af70630d181c5f1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-11 12:00:52 +0200

    write class and conversion function

--- controller/converter.js
+++ controller/converter.js
@@ -1,32 +1,101 @@
 const convert = require("color-convert");
 
-exports.keywordRGBHandler = (req, res) => {
-  const color = convert.keyword.rgb(req.query.color);
-  const [red, green, blue] = color;
-  return res.json({ red, green, blue });
-};
+exports.conversion = (req, res, from, to) => {
+  const { color } = req.query;
+  const convertedColor = new ColorConverter(from, to, color);
+  return res.json(convertedColor);
+}
 
-exports.RGB2HSLHandler = (req, res) => {
-  const colorInput = req.query.color
-    .split(",")
-    .map(channel => parseInt(channel));
-  const [hue, saturation, luminance] = convert.rgb.hsl(...colorInput);
-  return res.json({ hue, saturation, luminance });
-};
+class ColorConverter {
+  constructor(from, to, colorInput) {
+    this.from = from;
+    this.to = to;
+    this.colorInput = colorInput;
+    return this.convert();
+  }
+  convert() {
+    switch (this.from) {
+      case "keyword":
+        switch (this.to) {
+          case "rgb":
+            return this.keywordRGBHandler(this.colorInput)
+          default:
+            return this.errorMessage(this.from, this.to, this.colorInput);
+        }
+      case "rgb":
+        switch (this.to) {
+          case "hsl":
+            return this.RGB2HSLHandler(this.colorInput);
+          case "hex":
+            return this.RGB2HexHandler(this.colorInput);
+          default:
+            return this.errorMessage(this.from, this.to, this.colorInput);
+        }
+      case "hex":
+        switch (this.to) {
+          case "rgb":
+            return this.Hex2RGBHandler(this.colorInput);
+          default:
+            return this.errorMessage(this.from, this.to, this.colorInput);
+        }
+      default:
+        return this.errorMessage(this.from, this.to, this.colorInput);
+    }
+  }
+  errorMessage(from, to, input) {
+    return { error: { message: `The requested conversion of ${input} from ${from} to ${to} is currently not supported.` } }
+  }
+  keywordRGBHandler(colorInput) {
+    const color = convert.keyword.rgb(colorInput);
+    const [red, green, blue] = color;
+    return { red, green, blue };
+  }
+  RGB2HSLHandler(colorInput) {
+    const [red, green, blue] = this.splitColors(colorInput);
+    const [hue, saturation, luminance] = convert.rgb.hsl(red, green, blue);
+    return { hue, saturation, luminance };
+  }
+  RGB2HexHandler(colorInput) {
+    const [red, green, blue] = this.splitColors(colorInput);
+    const hex = convert.rgb.hex(red, green, blue);
+    return { hex };
+  }
+  Hex2RGBHandler(colorInput) {
+    const [red, green, blue] = convert.hex.rgb(colorInput);
+    return { red, green, blue };
+  }
+  splitColors(colorInput) {
+    return colorInput.split(",").map(channel => parseInt(channel));
+  }
+}
 
-exports.RGB2HexHandler = (req, res) => {
-  const colorInput = req.query.color
-    .split(",")
-    .map(channel => parseInt(channel));
-  const hex = convert.rgb.hex(...colorInput);
-  return res.json({ hex });
-};
+// exports.keywordRGBHandler = (req, res) => {
+//   const color = convert.keyword.rgb(req.query.color);
+//   const [red, green, blue] = color;
+//   return res.json({ red, green, blue });
+// };
 
-exports.Hex2RGBHandler = (req, res) => {
-  const { color: hex } = req.query;
-  const [r, g, b] = convert.hex.rgb(hex);
-  return res.json({ red: r, green: g, blue: b });
-};
+// exports.RGB2HSLHandler = (req, res) => {
+//   const colorInput = req.query.color
+//     .split(",")
+//     .map(channel => parseInt(channel));
+//   const [hue, saturation, luminance] = convert.rgb.hsl(...colorInput);
+//   return res.json({ hue, saturation, luminance });
+// };
+
+// exports.RGB2HexHandler = (req, res) => {
+//   const colorInput = req.query.color
+//     .split(",")
+//     .map(channel => parseInt(channel));
+//   const hex = convert.rgb.hex(...colorInput);
+//   return res.json({ hex });
+// };
+
+// exports.Hex2RGBHandler = (req, res) => {
+//   const { color: hex } = req.query;
+//   const [r, g, b] = convert.hex.rgb(hex);
+//   return res.json({ red: r, green: g, blue: b });
+// };
 
 // exports.readStatistics = (req, res) => {
 //   const fs = require("fs");
