aee0e63dfa753317de59097a70bfd25e81a48df9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-11 14:26:45 +0200

    test rgb to hsl

--- controller/converter.test.js
+++ controller/converter.test.js
@@ -21,3 +21,23 @@ describe("Test keyword to RGB conversion", () => {
     expect(new ColorConverter("keyword", "rgb", "blue")).toEqual({ red: 0, green: 0, blue: 255 });
   });
 });
+describe("Test RGB to HSL conversion", () => {
+  test("converts hotpink", () => {
+    expect(new ColorConverter("rgb", "hsl", "255,105,180")).toEqual({ hue: 330, saturation: 100, luminance: 71 });
+  });
+  test("converts white", () => {
+    expect(new ColorConverter("rgb", "hsl", "255,255,255")).toEqual({ hue: 0, saturation: 0, luminance: 100 });
+  });
+  test("converts black", () => {
+    expect(new ColorConverter("rgb", "hsl", "0,0,0")).toEqual({ hue: 0, saturation: 0, luminance: 0 });
+  });
+  test("converts red", () => {
+    expect(new ColorConverter("rgb", "hsl", "255,0,0")).toEqual({ hue: 0, saturation: 100, luminance: 50 });
+  });
+  test("converts green", () => {
+    expect(new ColorConverter("rgb", "hsl", "0,128,0")).toEqual({ hue: 120, saturation: 100, luminance: 25 });
+  });
+  test("converts blue", () => {
+    expect(new ColorConverter("rgb", "hsl", "0,0,255")).toEqual({ hue: 240, saturation: 100, luminance: 50 });
+  });
+});
