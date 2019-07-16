5bc8c72babfecd19568b18e2009ae1a135729efb
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-16 10:24:42 +0200

    use messaging functions

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const convertRoute = require("./router/convert");
 const { logMethod } = require("./controller/converter");
 // const { readStatistics } = require("./controller/converter");
 // const { readStatistics } = require("./model/statistics");
+const { showVersion, showHelp } = require("./messaging");
+
+const nodeParameters = process.argv.slice(2);
+if (nodeParameters.includes("--version")) {
+  showVersion();
+  process.exit();
+}
+
+if (nodeParameters.includes("--help")) {
+  showHelp();
+  process.exit();
+}
+
 
 app.use(logMethod);
 app.use("/convert", convertRoute);
